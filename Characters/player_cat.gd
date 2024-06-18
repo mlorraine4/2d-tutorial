7b4b851cde81b62cdd09a4bea6a1742d1d2f44e0
Garrett <sprintepic1122@gmail.com>
2024-06-18 18:57:40 -0400

    bla

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
+enum MovementModes {
+	IDLE,
+	WALK
+}
+
 #@export makes variable visible to inspector
 @export var move_speed : float = 100
 
 @onready var animation_tree = $AnimationTree
+@onready var animation_mode = animation_tree.get("parameters/playback")
 
 var direction : Vector2 = Vector2.ZERO
+var movement_mode : MovementModes = MovementModes.IDLE
+
+
 
 func _ready():
 	animation_tree.active = true
@@ -15,26 +24,37 @@ func _physics_process(_delta):
 	direction = Input.get_vector("left", "right", "up", "down").normalized()
 	
 	#Set velocity (characterbody already has a velocity var)
+
 	if direction:
-		velocity = direction * move_speed
+		set_velocity(direction * move_speed)
 	else:
-		velocity = Vector2.ZERO
+		set_velocity(Vector2.ZERO)
 	
+	print_debug("velocity:", velocity)
 	# Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()
 	
 	update_animation_parameters()
 	
 func update_animation_parameters():
+	var real_velocity = get_real_velocity()
+	if real_velocity.length()/move_speed < .07:
+		real_velocity = Vector2.ZERO
+	print_debug("Real velocity:", real_velocity.length()/move_speed)
+	
 	#determine if player is idle, walking, or pressed swing
-	animation_tree.set("parameters/conditions/idle", velocity == Vector2.ZERO)
-	animation_tree.set("parameters/conditions/walk", velocity != Vector2.ZERO)
+	if real_velocity == Vector2.ZERO:
+		movement_mode = MovementModes.IDLE
+	else:
+		movement_mode = MovementModes.WALK
+
+	if(velocity != Vector2.ZERO):
+		#if character moves, update direction for each animation
+		animation_tree["parameters/Swing/blend_position"] = velocity
+		animation_tree["parameters/Idle/blend_position"] = velocity
+		animation_tree["parameters/Walk/blend_position"] = velocity	
+
 	animation_tree.set("parameters/conditions/swing", Input.is_action_just_pressed("use"))
 	
-	if(direction != Vector2.ZERO):
-		#if character moves, update direction for each animation
-		animation_tree["parameters/Swing/blend_position"] = direction
-		animation_tree["parameters/Idle/blend_position"] = direction
-		animation_tree["parameters/Walk/blend_position"] = direction
-	
-
+	if Input.is_action_just_pressed("use"):
+		pass
