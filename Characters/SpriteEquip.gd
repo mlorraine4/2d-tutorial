e0c98b4a72b491d6e673ace9727dc0cb1fb06788
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-06-27 11:53:37 -0400

    finished tutorial collisions, water, layers

--- Characters/SpriteEquip.gd
+++ Characters/SpriteEquip.gd
@@ -6,7 +6,7 @@ extends Sprite2D
 		equipped_item = next_equipped
 		self.texture = next_equipped.texture
 
-#func connected to the area2D
+#func connected to the area2D, interact with body comes from harvesting_tool, which is a script for pickaxe resource
 func _on_area_2d_body_entered(body):
 	if(equipped_item.has_method("interact_with_body")):
 		equipped_item.interact_with_body(body)
