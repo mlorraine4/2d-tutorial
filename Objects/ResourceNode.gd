681718bcfd664ad92c65185e6070020b1f3898fb
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-07-01 11:26:50 -0400

    finished particle effect tutorial

--- Objects/ResourceNode.gd
+++ Objects/ResourceNode.gd
@@ -7,7 +7,7 @@ class_name ResourceNode
 @export var pickup_type : PackedScene
 @export var launch_speed : float = 100
 @export var launch_duration : float = 0.25
-
+@export var depleted_effect : PackedScene
 @onready var level_parent = get_parent()
 
 var current_resources : int :
@@ -16,6 +16,11 @@ var current_resources : int :
 		
 		#A resource node with no resources is removed from the scene
 		if(value <= 0):
+			#spawn particle effect before removing node from scene
+			var effect_instance : GPUParticles2D = depleted_effect.instantiate()
+			effect_instance.position = position
+			level_parent.add_child(effect_instance)
+			effect_instance.emitting = true
 			queue_free()
 
 func _ready():
